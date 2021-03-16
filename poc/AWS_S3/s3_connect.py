fd613f678b25f1fdbf61ddcb7f807513fba45f91
rsquare2010 <rahulceg1092@gmail.com>
2021-03-16 15:49:58 -0400

    poc code to push a json object to a bucket in S3

--- poc/AWS_S3/s3_connect.py
+++ poc/AWS_S3/s3_connect.py
@@ -28,6 +28,7 @@ for bucket in response['Buckets']:
 bucket = 'completed-bucket'
 key = '1234.json'
 
+
 # Fetch an object or print error if it doesnt exist.
 try :
     response = s3.get_object(Bucket=bucket, Key=key)
@@ -41,6 +42,15 @@ except ClientError as ex:
         raise
 
 region = 'us-east-2'
-# location = {'LocationConstraint': region}
-# s3_client = boto3.client('s3',)
 s3.create_bucket(ACL='private', Bucket="pend201023")
+
+
+# Upload a json object to S3
+json_string = '{"qobj_id": "e533279d-3cd7-4bcd-a5a8-962f3547f899", "header": {}, "config": {"shots": 1024, "memory": false, "parameter_binds": [], "init_qubits": true, "memory_slots": 2, "n_qubits": 2}, "schema_version": "1.3.0", "type": "QASM", "experiments": [{"config": {"n_qubits": 2, "memory_slots": 2}, "header": {"qubit_labels": [["q", 0], ["q", 1]], "n_qubits": 2, "qreg_sizes": [["q", 2]], "clbit_labels": [["c", 0], ["c", 1]], "memory_slots": 2, "creg_sizes": [["c", 2]], "name": "circuit7", "global_phase": 0.0}, "instructions": [{"name": "u2", "params": [0.0, 3.141592653589793], "qubits": [0]}, {"name": "cx", "qubits": [0, 1]}, {"name": "measure", "qubits": [0], "memory": [0]}, {"name": "measure", "qubits": [1], "memory": [1]}]}]}'
+json_body = json.loads(json_string)
+
+s3.put_object(
+     Body=str(json.dumps(json_body)),
+     Bucket=bucket,
+     Key='4568.json'
+)
